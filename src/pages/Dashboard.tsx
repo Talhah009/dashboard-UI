59a2b96412a3a576022d233d95649df9dc87dcb7
Talhah009 <talhaejaz889@gmail.com>
2024-03-27 16:51:20 +0500

    working...

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard Page Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure aliquam dicta sint modi eius repudiandae ut cumque reiciendis sapiente debitis totam impedit fugiat molestiae facilis asperiores, placeat voluptate laborum? Totam itaque tempore omnis, rerum a architecto odio fuam mollitia, architecto deserunt eos doloribus ducimus facere dodio corrupti laudantium exercitationem accusantium, dolorum possimus! Aliquam quod perferendis quam, sed voluptatem commodi ipsam facere enim alias corrupti ratione.</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
